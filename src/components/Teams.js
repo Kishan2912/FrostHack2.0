b43c71ed6400521255cae90c7bd340aab18f27bd
Rijul Jain <jrijul1201@gmail.com>
2022-01-15 16:21:22 +0530

    Fixed warning

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -23,10 +23,10 @@ export const Teams = (props) => {
         <br />
         <h2><b className="changecolor">Saransh Jain</b></h2>
         <p className="changecolor">Overall Head</p>
-        <a href="https://www.facebook.com/saransh.jain.921677" target="_blank">
+        <a rel="noreferrer" href="https://www.facebook.com/saransh.jain.921677" target="_blank">
           <i className="fab fa-facebook fa-lg" />
         </a>
-        <a href="tel:+917300067222" target="_blank">
+        <a rel="noreferrer" href="tel:+917300067222" target="_blank">
           <i className="fas fa-phone fa-lg" />
         </a>
       </div>
@@ -37,10 +37,10 @@ export const Teams = (props) => {
         <br />
         <h2><b className="changecolor">Kartik Kathuria</b></h2>
         <p className="changecolor">Overall Head</p>
-        <a href="https://www.facebook.com/kartik.kathuria.146" target="_blank">
+        <a rel="noreferrer" href="https://www.facebook.com/kartik.kathuria.146" target="_blank">
           <i className="fab fa-facebook fa-lg" />
         </a>
-        <a href="tel:+917982453395" target="_blank">
+        <a rel="noreferrer" href="tel:+917982453395" target="_blank">
           <i className="fas fa-phone fa-lg" />
         </a>
       </div>
@@ -51,10 +51,10 @@ export const Teams = (props) => {
         <br />
         <h2><b className="changecolor">Subhash Suman</b></h2>
         <p className="changecolor">Overall Head</p>
-        <a href="https://www.facebook.com/subhash.suman.186" target="_blank">
+        <a rel="noreferrer" href="https://www.facebook.com/subhash.suman.186" target="_blank">
           <i className="fab fa-facebook fa-lg" />
         </a>
-        <a href="tel:+917703813759" target="_blank">
+        <a rel="noreferrer" href="tel:+917703813759" target="_blank">
           <i className="fas fa-phone fa-lg" />
         </a>
       </div>
@@ -65,10 +65,10 @@ export const Teams = (props) => {
         <br />
         <h2><b className="changecolor">Vyom Goel</b></h2>
         <p className="changecolor">Overall Head</p>
-        <a href="https://www.facebook.com/vyom.goel.12" target="_blank">
+        <a rel="noreferrer" href="https://www.facebook.com/vyom.goel.12" target="_blank">
           <i className="fab fa-facebook fa-lg" />
         </a>
-        <a href="tel:+919816790032" target="_blank">
+        <a rel="noreferrer" href="tel:+919816790032" target="_blank">
           <i className="fas fa-phone fa-lg" />
         </a>
       </div>
@@ -79,10 +79,10 @@ export const Teams = (props) => {
         <br />
         <h2><b className="changecolor">Vasu Gupta</b></h2>
         <p className="changecolor">Finance Head</p>
-        <a href="https://www.facebook.com/vgzelda123" target="_blank">
+        <a rel="noreferrer" href="https://www.facebook.com/vgzelda123" target="_blank">
           <i className="fab fa-facebook fa-lg" />
         </a>
-        <a href="tel:+917009300207" target="_blank">
+        <a rel="noreferrer" href="tel:+917009300207" target="_blank">
           <i className="fas fa-phone fa-lg" />
         </a>
       </div>
@@ -93,10 +93,10 @@ export const Teams = (props) => {
         <br />
         <h2><b className="changecolor">Vipul Sharma</b></h2>
         <p className="changecolor">Finance</p>
-        <a href="https://www.facebook.com/vsvipul555" target="_blank">
+        <a rel="noreferrer" href="https://www.facebook.com/vsvipul555" target="_blank">
           <i className="fab fa-facebook fa-lg" />
         </a>
-        <a href="tel:+919418322955" target="_blank">
+        <a rel="noreferrer" href="tel:+919418322955" target="_blank">
           <i className="fas fa-phone fa-lg" />
         </a>
       </div>
@@ -107,10 +107,10 @@ export const Teams = (props) => {
         <br />
         <h2><b className="changecolor">Abhijeet Manhas</b></h2>
         <p className="changecolor">Technical</p>
-        <a href="https://www.facebook.com/astromanahas" target="_blank">
+        <a rel="noreferrer" href="https://www.facebook.com/astromanahas" target="_blank">
           <i className="fab fa-facebook fa-lg" />
         </a>
-        <a href="tel:+918628963924" target="_blank">
+        <a rel="noreferrer" href="tel:+918628963924" target="_blank">
           <i className="fas fa-phone fa-lg" />
         </a>
       </div>
@@ -121,10 +121,10 @@ export const Teams = (props) => {
         <br />
         <h2><b className="changecolor">Saksham Barara</b></h2>
         <p className="changecolor">Branding</p>
-        <a href="https://www.facebook.com/profile.php?id=100011143120838" target="_blank">
+        <a rel="noreferrer" href="https://www.facebook.com/profile.php?id=100011143120838" target="_blank">
           <i className="fab fa-facebook fa-lg" />
         </a>
-        <a href="tel:+919877757063" target="_blank">
+        <a rel="noreferrer" href="tel:+919877757063" target="_blank">
           <i className="fas fa-phone fa-lg" />
         </a>
       </div>
@@ -135,10 +135,10 @@ export const Teams = (props) => {
         <br />
         <h2><b className="changecolor">Shobhit Rajput</b></h2>
         <p className="changecolor">Branding</p>
-        <a href="https://www.facebook.com/shobhit.rajput.391" target="_blank">
+        <a rel="noreferrer" href="https://www.facebook.com/shobhit.rajput.391" target="_blank">
           <i className="fab fa-facebook fa-lg" />
         </a>
-        <a href="tel:+918126588271" target="_blank">
+        <a rel="noreferrer" href="tel:+918126588271" target="_blank">
           <i className="fas fa-phone fa-lg" />
         </a>
       </div>
@@ -149,10 +149,10 @@ export const Teams = (props) => {
         <br />
         <h2><b className="changecolor">Priyam Seth</b></h2>
         <p className="changecolor">Branding</p>
-        <a href="https://m.facebook.com/priyam.seth.336" target="_blank">
+        <a rel="noreferrer" href="https://m.facebook.com/priyam.seth.336" target="_blank">
           <i className="fab fa-facebook fa-lg" />
         </a>
-        <a href="tel:+919811417932" target="_blank">
+        <a rel="noreferrer" href="tel:+919811417932" target="_blank">
           <i className="fas fa-phone fa-lg" />
         </a>
       </div>
@@ -163,10 +163,10 @@ export const Teams = (props) => {
         <br />
         <h2><b className="changecolor">Vineet Ahuja</b></h2>
         <p className="changecolor">Branding</p>
-        <a href="https://www.facebook.com/vineet.ahuja.378" target="_blank">
+        <a rel="noreferrer" href="https://www.facebook.com/vineet.ahuja.378" target="_blank">
           <i className="fab fa-facebook fa-lg" />
         </a>
-        <a href="tel:+919911473244" target="_blank">
+        <a rel="noreferrer" href="tel:+919911473244" target="_blank">
           <i className="fas fa-phone fa-lg" />
         </a>
       </div>
@@ -177,10 +177,10 @@ export const Teams = (props) => {
         <br />
         <h2><b className="changecolor">Shubhanshu Agrawal</b></h2>
         <p className="changecolor">Branding</p>
-        <a href="https://m.facebook.com/profile.php?id=100014538347418&ref=content_filter" target="_blank">
+        <a rel="noreferrer" href="https://m.facebook.com/profile.php?id=100014538347418&ref=content_filter" target="_blank">
           <i className="fab fa-facebook fa-lg" />
         </a>
-        <a href="tel:+917987590764" target="_blank">
+        <a rel="noreferrer" href="tel:+917987590764" target="_blank">
           <i className="fas fa-phone fa-lg" />
         </a>
       </div>
@@ -191,10 +191,10 @@ export const Teams = (props) => {
         <br />
         <h2><b className="changecolor">Shubham Saurav</b></h2>
         <p className="changecolor">Branding</p>
-        <a href="https://www.facebook.com/shubham.saurav.980967" target="_blank">
+        <a rel="noreferrer" href="https://www.facebook.com/shubham.saurav.980967" target="_blank">
           <i className="fab fa-facebook fa-lg" />
         </a>
-        <a href="tel:+919304902560" target="_blank">
+        <a rel="noreferrer" href="tel:+919304902560" target="_blank">
           <i className="fas fa-phone fa-lg" />
         </a>
       </div>
@@ -205,10 +205,10 @@ export const Teams = (props) => {
         <br />
         <h2><b className="changecolor">Priyanshu Shubham</b></h2>
         <p className="changecolor">Branding</p>
-        <a href="https://www.facebook.com/priyanshu.shubham3" target="_blank">
+        <a rel="noreferrer" href="https://www.facebook.com/priyanshu.shubham3" target="_blank">
           <i className="fab fa-facebook fa-lg" />
         </a>
-        <a href="tel:+918340437870" target="_blank">
+        <a rel="noreferrer" href="tel:+918340437870" target="_blank">
           <i className="fas fa-phone fa-lg" />
         </a>
       </div>
@@ -219,10 +219,10 @@ export const Teams = (props) => {
         <br />
         <h2><b className="changecolor">Pratiksha Jain</b></h2>
         <p className="changecolor">Branding</p>
-        <a href="https://www.facebook.com/profile.php?id=100006780016974" target="_blank">
+        <a rel="noreferrer" href="https://www.facebook.com/profile.php?id=100006780016974" target="_blank">
           <i className="fab fa-facebook fa-lg" />
         </a>
-        <a href="tel:+918130476897" target="_blank">
+        <a rel="noreferrer" href="tel:+918130476897" target="_blank">
           <i className="fas fa-phone fa-lg" />
         </a>
       </div>
@@ -233,10 +233,10 @@ export const Teams = (props) => {
         <br />
         <h2><b className="changecolor">Divyasheel</b></h2>
         <p className="changecolor">Technical</p>
-        <a href="https://www.facebook.com/divyasheel.kumar/" target="_blank">
+        <a rel="noreferrer" href="https://www.facebook.com/divyasheel.kumar/" target="_blank">
           <i className="fab fa-facebook fa-lg" />
         </a>
-        <a href="tel:+918437151118" target="_blank">
+        <a rel="noreferrer" href="tel:+918437151118" target="_blank">
           <i className="fas fa-phone fa-lg" />
         </a>
       </div>
@@ -247,10 +247,10 @@ export const Teams = (props) => {
         <br />
         <h2><b className="changecolor">Pranshu Kharkwal</b></h2>
         <p className="changecolor">Branding</p>
-        <a href="https://www.facebook.com/kharkwalpranshu" target="_blank">
+        <a rel="noreferrer" href="https://www.facebook.com/kharkwalpranshu" target="_blank">
           <i className="fab fa-facebook fa-lg" />
         </a>
-        <a href="tel:+917876219654" target="_blank">
+        <a rel="noreferrer" href="tel:+917876219654" target="_blank">
           <i className="fas fa-phone fa-lg" />
         </a>
       </div>
@@ -261,10 +261,10 @@ export const Teams = (props) => {
         <br />
         <h2><b className="changecolor">Deepali Singh</b></h2>
         <p className="changecolor">Branding</p>
-        <a href="https://www.facebook.com/deepali.s11/" target="_blank">
+        <a rel="noreferrer" href="https://www.facebook.com/deepali.s11/" target="_blank">
           <i className="fab fa-facebook fa-lg" />
         </a>
-        <a href="tel:+918058041116" target="_blank">
+        <a rel="noreferrer" href="tel:+918058041116" target="_blank">
           <i className="fas fa-phone fa-lg" />
         </a>
       </div>
@@ -275,10 +275,10 @@ export const Teams = (props) => {
         <br />
         <h2><b className="changecolor">Aditya Sarkar</b></h2>
         <p className="changecolor">Technical</p>
-        <a href target="_blank">
+        <a rel="noreferrer" href target="_blank">
           <i className="fab fa-facebook fa-lg" />
         </a>
-        <a href="tel:+917042728328" target="_blank">
+        <a rel="noreferrer" href="tel:+917042728328" target="_blank">
           <i className="fas fa-phone fa-lg" />
         </a>
       </div>
