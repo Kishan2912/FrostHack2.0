6cce600291db698bd9dcffa6e6a345997f07f516
dipesh809 <b20096@students.iitmandi.ac.in>
2022-01-16 17:52:34 +0530

    changed theme of team and mentors page

--- public/js/index.js
+++ public/js/index.js
@@ -1,139 +1,180 @@
 
-function lightmode() {
-    document.body.style.backgroundColor = "#FFFFFF";
-    var x = document.getElementsByClassName("title");
-    var i;
-    for (i = 0; i < x.length; i++) {
-        x[i].style.color = "black";
-    }
-    var x = document.getElementsByClassName("mt-3");
-    var i;
-    for (i = 0; i < x.length; i++) {
-        x[i].style.color = "black";
-    }
-    var x = document.getElementsByClassName("changecolor");
-    var i;
-    for (i = 0; i < x.length; i++) {
-        x[i].style.color = "black";
-    }
-    var x = document.getElementsByClassName("fa-map-marked-alt");
-    var i;
-    for (i = 0; i < x.length; i++) {
-        x[i].style.color = "black";
-    }
+function darkmodeteam(){
+    document.body.style.backgroundColor = "#0F131C";
+    document.getElementById("theme-toggle").src = "img/moon.svg";
     var x = document.getElementsByClassName("fa-facebook");
     var i;
     for (i = 0; i < x.length; i++) {
-        x[i].style.color = "black";
+          x[i].style.color = "white";
     }
     var x = document.getElementsByClassName("fa-instagram");
     var i;
     for (i = 0; i < x.length; i++) {
-        x[i].style.color = "black";
+          x[i].style.color = "white";
     }
     var x = document.getElementsByClassName("fa-phone");
     var i;
     for (i = 0; i < x.length; i++) {
-        x[i].style.color = "black";
+          x[i].style.color = "white";
     }
-    var x = document.getElementsByClassName("fa-envelope");
+    var x = document.getElementsByClassName("fa-linkedin");
     var i;
     for (i = 0; i < x.length; i++) {
-        x[i].style.color = "black";
+          x[i].style.color = "white";
     }
     var x = document.getElementsByClassName("fa-discord");
     var i;
     for (i = 0; i < x.length; i++) {
-        x[i].style.color = "black";
+          x[i].style.color = "white";
     }
-    var x = document.getElementsByClassName("panel");
+    var x = document.getElementsByClassName("fa-envelope");
     var i;
     for (i = 0; i < x.length; i++) {
-        x[i].style.backgroundColor = "white";
-        x[i].style.borderColor = "white";
+          x[i].style.color = "white";
     }
-    document.getElementById("cards_landscape_wrap-2").style.backgroundColor = "white";
-    document.getElementById("theme-toggle").src = "img/sun.svg";
-    document.getElementById("code_of_conduct").style.color = "#000000"
+    var x = document.getElementsByClassName("fa-map-marked-alt");
+    var i;
+    for (i = 0; i < x.length; i++) {
+          x[i].style.color = "white";
+    }
+    var x = document.getElementsByClassName("changecolor");
+    var i;
+    for (i = 0; i < x.length; i++) {
+          x[i].style.color = "white";
+    }
+    document.getElementById("nav-logo").src = "./img/logos/wide-small-white.png";
+    $("#code_of_conduct").style.color = "#ffffff";
 }
-function darkmode() {
+
+function darkmodementor(){
+    document.body.style.backgroundColor = "#0F131C";
+    document.getElementById("theme-toggle").src = "img/moon.svg";
+    var x = document.getElementsByClassName("fa-facebook");
+    var i;
+    for (i = 0; i < x.length; i++) {
+          x[i].style.color = "white";
+    }
+    var x = document.getElementsByClassName("fa-instagram");
+    var i;
+    for (i = 0; i < x.length; i++) {
+          x[i].style.color = "white";
+    }
+    var x = document.getElementsByClassName("fa-phone");
+    var i;
+    for (i = 0; i < x.length; i++) {
+          x[i].style.color = "white";
+    }
+    var x = document.getElementsByClassName("fa-linkedin");
+    var i;
+    for (i = 0; i < x.length; i++) {
+          x[i].style.color = "white";
+    }
+    var x = document.getElementsByClassName("fa-discord");
+    var i;
+    for (i = 0; i < x.length; i++) {
+          x[i].style.color = "white";
+    }
+    var x = document.getElementsByClassName("fa-envelope");
+    var i;
+    for (i = 0; i < x.length; i++) {
+          x[i].style.color = "white";
+    }
+    var x = document.getElementsByClassName("fa-map-marked-alt");
+    var i;
+    for (i = 0; i < x.length; i++) {
+          x[i].style.color = "white";
+    }
+    var x = document.getElementsByClassName("changecolor");
+    var i;
+    for (i = 0; i < x.length; i++) {
+          x[i].style.color = "white";
+    }
+    document.getElementById("nav-logo").src = "./img/logos/wide-small-white.png";
+    document.getElementById("code_of_conduct").style.color = "#ffffff";
+}
+
+function darkmodeindex() {
     document.body.style.backgroundColor = "#0F131C";
     document.getElementById("about").style.color = "#FFFFFF";
     var x = document.getElementsByClassName("title");
     var i;
     for (i = 0; i < x.length; i++) {
-        x[i].style.color = "#FFFFFF";
+          x[i].style.color = "#FFFFFF";
     }
     var x = document.getElementsByClassName("mt-3");
     var i;
     for (i = 0; i < x.length; i++) {
-        x[i].style.color = "#FFFFFF";
+          x[i].style.color = "#FFFFFF";
     }
     var x = document.getElementsByClassName("changecolor");
     var i;
     for (i = 0; i < x.length; i++) {
-        x[i].style.color = "white";
+          x[i].style.color = "white";
     }
     var x = document.getElementsByClassName("fa-facebook");
     var i;
     for (i = 0; i < x.length; i++) {
-        x[i].style.color = "white";
+          x[i].style.color = "white";
     }
     var x = document.getElementsByClassName("fa-instagram");
     var i;
     for (i = 0; i < x.length; i++) {
-        x[i].style.color = "white";
+          x[i].style.color = "white";
     }
     var x = document.getElementsByClassName("fa-map-marked-alt");
     var i;
     for (i = 0; i < x.length; i++) {
-        x[i].style.color = "white";
+          x[i].style.color = "white";
     }
     var x = document.getElementsByClassName("fa-phone");
     var i;
     for (i = 0; i < x.length; i++) {
-        x[i].style.color = "white";
+          x[i].style.color = "white";
     }
     var x = document.getElementsByClassName("fa-envelope");
     var i;
     for (i = 0; i < x.length; i++) {
-        x[i].style.color = "white";
+          x[i].style.color = "white";
     }
     var x = document.getElementsByClassName("fa-discord");
     var i;
     for (i = 0; i < x.length; i++) {
-        x[i].style.color = "white";
+          x[i].style.color = "white";
     }
     var x = document.getElementsByClassName("card-flyer");
     var i;
     for (i = 0; i < x.length; i++) {
-        x[i].style.borderColor = "white";
+          x[i].style.borderColor = "white";
     }
     var x = document.getElementsByClassName("panel");
     var i;
     for (i = 0; i < x.length; i++) {
-        x[i].style.backgroundColor = "#0F131C";
+          x[i].style.backgroundColor = "#0F131C";
         x[i].style.borderColor = "#0F131C";
     }
     document.getElementById("cards_landscape_wrap-2").style.backgroundColor = "#0F131C";
     document.getElementById("theme-toggle").src = "img/moon.svg";
-    document.getElementById("code_of_conduct").style.color = "#ffffff"
+    document.getElementById("code_of_conduct").style.color = "#ffffff";
 }
-function themechange() {
-    if (document.body.style.backgroundColor == "rgb(15, 19, 28)") {
-        lightmode();
-    }
-    else {
-        darkmode();
-    }
-}
+
 var path= window.location.pathname
-if(path === "/judges" || path=== "/mentors" || path=== "/teams" || path==="/code"){
+if(path === "/judges" || path==="/code"){
     window.onload = function () {
         lightmode();
+    };
+}else if(path=== "/mentors" ){
+    window.onload = function () {
+        darkmodementor();
+        $("#code_of_conduct").style.color = "#ffffff";
+    };
+}else if(path=== "/teams"){
+    window.onload = function () {
+        darkmodeteam();
+        $("#code_of_conduct").style.color = "#ffffff";
     };
 }else{
     window.onload = function () {
-        darkmode();
-    };
+        darkmodeindex();
+        $("#code_of_conduct").style.color = "#ffffff";
+    }
 }
